63741349412a254a66a0cde8befd78873d0db189
mizhoux <mizhoux@gmail.com>
2019-10-09 15:50:04 +0800

    enhance Switch

--- src/main/java/xyz/mizhoux/sugar/Switch.java
+++ src/main/java/xyz/mizhoux/sugar/Switch.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
  * Switch
@@ -117,6 +118,33 @@ public class Switch<T, R> {
         }
 
         return this;
+    }
+
+    public R elseApply(Function<T, R> mapper) {
+        Objects.requireNonNull(mapper);
+
+        return met ? output : mapper.apply(input);
+    }
+
+    public Switch<T, R> thenSupply(Supplier<R> supplier) {
+        if (met) { return this; }
+
+        requireNonNullArgAndCondition(supplier);
+
+        if (condition.test(input)) {
+            output = supplier.get();
+
+            // 标记已经存在输出
+            met = true;
+        }
+
+        return this;
+    }
+
+    public R elseSupply(Supplier<R> supplier) {
+        Objects.requireNonNull(supplier);
+
+        return met ? output : supplier.get();
     }
 
     public Switch<T, R> or(T target) {
